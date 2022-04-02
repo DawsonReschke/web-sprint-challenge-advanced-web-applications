54fe5e4f881421bb26b5f839e759d4039067a57b
dawson <dawsonreschke@icloud.com>
2022-04-01 17:05:02 -0700

    added spinner tests

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,22 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
+import React from 'react'
+import { render, fireEvent, screen, waitForElementToBeRemoved } from '@testing-library/react'
+import Spinner from './Spinner'
+
+const spinner = ()=> document.querySelector('#spinner')
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+
+
+test('spinner has correct opacity when NOT active',async()=>{
+  render(<Spinner on={false}/>)
+  expect(spinner()).toBeFalsy()
+})
+test('spinner has correct opacity when active',async()=>{
+  render(<Spinner on={true}/>)
+  expect(spinner()).toBeTruthy()
 })
